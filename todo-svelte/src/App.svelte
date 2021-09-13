8c7f1049edd3a5bda9dbbc0f3b390bd68162a2c0
EssaifiAahmed <a.essaifi12@gmail.com>
2021-09-13 16:57:15 +0100

    TodoApp

---
+++ todo-svelte/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import  Header  from "./components/header.svelte";
+	import  List  from "./components/List.svelte";
+</script>
+
+
+<style>
+	.container{
+		width: 500px;
+	}
+	:global(.container *) {
+		font-family: "Quicksand", sans-serif;
+		background-color: #009579;
+	}
+</style>
+
+<dev class="container">
+	<Header />
+	<List />
+</dev>
---
+++ todo-svelte/src/components/header.svelte
@@ -0,0 +1,24 @@
+<script>
+    import {items} from "../stores";
+    
+    $: itemCount = $items.length;
+    $: completedItemCount = $items.filter((item) => item.complete).length;
+</script>
+
+<style>
+    .header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 15px 20px;
+        color: #ffffff;
+        font-weight: bold;
+        font-size: 1.5em;
+        background: #009579;    
+    }
+</style>
+
+<dev class="header">
+    <span>Dom's To-Do List</span>
+    <span>{completedItemCount}/{itemCount}</span>
+</dev>
